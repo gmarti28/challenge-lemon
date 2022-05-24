4cd116921638e5df9b1a592417162cf0c19afa77
gaston.martin <gaston.martin@ank.app>
2022-05-24 11:56:18 -0300

    Initial commit

---
+++ src/main/kotlin/com/gastonmartin/challenges/lemon/LemonApplication.kt
@@ -0,0 +1,11 @@
+package com.gastonmartin.challenges.lemon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LemonApplication
+
+fun main(args: Array<String>) {
+	runApplication<LemonApplication>(*args)
+}
---
+++ src/test/kotlin/com/gastonmartin/challenges/lemon/LemonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gastonmartin.challenges.lemon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LemonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
